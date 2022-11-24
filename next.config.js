b0d420d1a28672f776bc1ea6921cca1f42680231
sirily11 <sirily1997@gmail.com>
2022-11-24 16:21:58 +0800

    fix: add apple-app-site-association

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    headerRewrite: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: "/.well-known/apple-app-site-association",
+        headers: [{ key: "content-type", value: "application/json" }],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
