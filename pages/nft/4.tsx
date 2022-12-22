66cc246c95c2b2969fac3402e6b1ab861071a1be
sirily11 <sirily1997@gmail.com>
2022-12-22 16:10:43 +0800

    fix: add page 4

---
+++ pages/nft/4.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CardMedia } from "@mui/material";
+
+export default function Index() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <img src={"/screenshot.JPG"} />
+    </div>
+  );
+}
